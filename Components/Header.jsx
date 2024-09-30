1888e413e0eda2ab1dc62a3596045230e17a4d06
Jadavnitin <nitinkumarjadav83@gmail.com>
2024-09-30 22:40:56 +0530

    Fixed layout issues

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -51,6 +51,30 @@ export default Header
 // `;
 
 
+// const HeaderDiv = styled.div`
+// width:80vmax;
+// min-height:10vh;
+// display: flex;
+// align-items: center;
+// flex-wrap: wrap;
+// justify-content: space-between;
+// row-gap: 10px;
+// button{
+   
+//    text-align: center;
+//    height: 60px;
+//    width: 148px;
+//    border: none;
+//    border-radius: 24px;
+//    box-shadow:-5px -5px 10px rgba(255, 255, 255, 0.25), 5px 5px 10px rgba(0,0 ,0,25) ;
+//    color:white;
+//    background-color:#24272C;
+//    font-size:22px;
+// }
+
+// `;
+
+
 const HeaderDiv = styled.div`
 width:80vmax;
 min-height:10vh;
@@ -58,7 +82,7 @@ display: flex;
 align-items: center;
 flex-wrap: wrap;
 justify-content: space-between;
-row-gap: 10px;
+row-gap: 10vmin;
 button{
    
    text-align: center;
@@ -68,7 +92,7 @@ button{
    border-radius: 24px;
    box-shadow:-5px -5px 10px rgba(255, 255, 255, 0.25), 5px 5px 10px rgba(0,0 ,0,25) ;
    color:white;
-   background-color:#24272C;
+   background-color:#1966e0;
    font-size:22px;
 }
 
