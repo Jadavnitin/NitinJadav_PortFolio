6fcb765d96fb55a90d5f1bace626823b7bb79e16
Jadavnitin <nitinkumarjadav83@gmail.com>
2024-10-01 04:22:46 +0530

    Update

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,24 +1,57 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const Header = () => {
+const Header = ({refs}) => {
    
  
    
+   
+   const scrollhandler = (section) => {
+      console.log(section)
+   window.scrollTo({top:section.current.offsetTop, behavior:"smooth"})
+ }
+   
    return (
       <HeaderDiv>
-         <button >Resume</button>
-         <button >About</button>
-         <button >Certificate</button>
-         <button >Projects</button>
-         <button >Contact</button>
+         <button onClick={() => scrollhandler(refs.ResumeSection)}>Resume</button>
+         <button onClick={() => scrollhandler(refs.AboutSection)}>About</button>
+         <button onClick={() => scrollhandler(refs.CertificateSection)}>Certificate</button>
+         <button onClick={()=>scrollhandler(refs.ProjectsSection)}>Projects</button>
+         <button onClick={()=>scrollhandler(refs.ContactSection)}>Contact</button>
          
-
       </HeaderDiv>
    )
 }
 
 export default Header
+
+
+
+const HeaderDiv = styled.div`
+width:80vmax;
+min-height:10vh;
+display: flex;
+align-items: center;
+flex-wrap: wrap;
+justify-content: space-between;
+row-gap: 10vmin;
+button{
+   
+   text-align: center;
+   height: 60px;
+   width: 148px;
+   border: none;
+   border-radius: 24px;
+   box-shadow:-5px -5px 10px rgba(255, 255, 255, 0.25), 5px 5px 10px rgba(0,0 ,0,25) ;
+   color:white;
+   background-color:#24272C;
+   font-size:22px;
+   cursor: pointer;
+}
+
+`;
+
+
 
 
 // const HeaderDiv = styled.header`
@@ -73,27 +106,3 @@ export default Header
 // }
 
 // `;
-
-
-const HeaderDiv = styled.div`
-width:80vmax;
-min-height:10vh;
-display: flex;
-align-items: center;
-flex-wrap: wrap;
-justify-content: space-between;
-row-gap: 10vmin;
-button{
-   
-   text-align: center;
-   height: 60px;
-   width: 148px;
-   border: none;
-   border-radius: 24px;
-   box-shadow:-5px -5px 10px rgba(255, 255, 255, 0.25), 5px 5px 10px rgba(0,0 ,0,25) ;
-   color:white;
-   background-color:#24272C;
-   font-size:22px;
-}
-
-`;
--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -1,77 +1,129 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Header from './Header';
+// import mainlogo from "../Images/MainLogo.jpg"
 import styled from 'styled-components';
-import mainlogo from "../Images/MainLogo.jpg"
-
+// import boylogo from "../Images/Boy.jpeg"
 
 
 const HomePage = () => {
-   
-   
+
+   const CertificateSection = useRef();
+   const ResumeSection = useRef();
+   const AboutSection = useRef();
+   const ProjectsSection = useRef();
+   const ContactSection = useRef();
+
    return (
-      <HomePageDiv>
-         <Main>
-         <Header />
-            <MainImg>
-               <img src={mainlogo} alt="mainlogo" />
-            </MainImg>
-            <Div>
-            <Hey>Hey</Hey>
-            <Name>I’m Nitin Jadav</Name>
-            <Profession>Computer<span>Engineer</span></Profession>
-            <Introduction>
-               Creative web developer skilled in HTML, CSS, and JavaScript,
-               dedicated to building engaging and user-friendly web  applications.
-               Eager to apply my problem-solving abilities and collaborative
-               mindset in an entry-level role.
-            </Introduction>
+      <>
+         <HomePageDiv>
+            <Main>
+               <Header refs={{ CertificateSection, ResumeSection, AboutSection, ProjectsSection, ContactSection }} />
+               <MainImg>
+               
+                     {/* <img src={mainlogo} alt="mainlogo" /> */}
             
-           <ButtonDiv>
-            <ButtonLearnMore>Learn More</ButtonLearnMore>
-               <ButtonContactMe>Contact Me</ButtonContactMe>
-            </ButtonDiv>
-            </Div>
-         </Main>
-      </HomePageDiv>
+               </MainImg>
+               <Div>
+                  <Hey>Hey</Hey>
+                  <Name>I’m Nitin Jadav</Name>
+                  <Profession>Computer<span>Engineer</span></Profession>
+                  <Introduction>
+                     Creative web developer skilled in HTML, CSS, and JavaScript,
+                     dedicated to building engaging and user-friendly web  applications.
+                     Eager to apply my problem-solving abilities and collaborative
+                     mindset in an entry-level role.
+                  </Introduction>
+
+                  <ButtonDiv>
+                     <ButtonLearnMore>Learn More</ButtonLearnMore>
+                     <ButtonContactMe>Contact Me</ButtonContactMe>
+                  </ButtonDiv>
+               </Div>
+
+
+               <AboutSections ref={AboutSection}>
+                  <AboutImageDiv>
+                     {/* <img src={boylogo} alt="boylogo" /> */}
+                  </AboutImageDiv>
+                  <AboutMeIntroDuction>
+                     <h1>About me </h1>
+                     <p>
+                        Hello! I’m Nitin Jadav, a passionate computer science graduate
+                        from Silver Oak University, Ahmedabad. Throughout my B.Tech journey,
+                        I developed a strong foundation in programming, algorithms, and software development.
+                        I am particularly interested in areas like web development, and artificial intelligence.
+
+                     </p>
+                  </AboutMeIntroDuction>
+               </AboutSections>
+               <CertificateSections ref={CertificateSection}>certificaate page</CertificateSections>
+               <ResumeSections ref={ResumeSection}>Resume Page</ResumeSections>
+               <ProjectsSections ref={ProjectsSection}>Projects Page</ProjectsSections>
+               <ContactSections ref={ContactSection}>Contact Page</ContactSections>
+            </Main>
+         </HomePageDiv>
+      </>
    )
 }
 
 export default HomePage
 
-// const HomePageDiv = styled.div`
-// display: flex;
-// justify-content: center;
-// align-items: center;
-// margin: 2vmax;
-// background-color: #232732;
-// box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
-// border-radius: 24px;
-// border: none;
-// color: white;
-// `;
-
-// const Main = styled.div`
-// display: flex;
-// justify-content: center;
-// align-items: center;
-// flex-direction: column;
-// padding: 70px 100px;
 
 
+const HomePageDiv = styled.section`
+margin-top: 10vmin;
+width: 90vmax;
+background-color: #232732;
+box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+border-radius: 24px;
 
-// `;
+`;
 
+const Main = styled.div`
+width:80vmax;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+gap: 40px;
+padding: 15vmin 40vmax ;
+
+`;
 // const MainImg = styled.div`
-// height: 474px;
-// width: 474px;
-// background-image: linear-gradient(rgba(255, 255, 255, 0.2),
-//  rgba(255, 255, 255, 0.2)),  url("../Images/MainLogo.jpg");
-// background-size: cover;
-// box-shadow:-5px -5px 10px rgba(255, 255, 255, 0.25), 5px 5px 10px rgba(255, 255, 255, 0.25) ;
-// border-radius: 100%;
-// background-position: center;
-// margin-top: 50px;
+
+// height: 35vh;
+// width: 35vmax;
+
+   
+//    img{
+//    width: 100%;
+//    border-radius: 100%;
+//   box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
+// }
+
+
+
 // `;
+
+
+const MainImg = styled.div`
+  height:40vmax;
+  width: 40vmax; 
+  border-radius: 100%; 
+  background-image: url('../Images/MainLogo.jpg'); 
+  background-size: cover; 
+  background-position: center; 
+  box-shadow: 8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0, 0, 0, 0.25);
+`;
+
+
+
+
+
 
 const Hey = styled.p`
 margin: 0;
@@ -125,7 +177,7 @@ align-items: center;
 margin: 0;
 padding: 0;
 font-size: 21px;
-font-weight: light;
+font-weight: lighter;
 text-align: center;
 
 
@@ -166,7 +218,6 @@ const ButtonContactMe = styled.button`
 `;
 
 const Div = styled.div`
-margin-top: 35vmin;
 display:flex;
 justify-content: center;
 align-items: center;
@@ -177,43 +228,181 @@ gap: 20px;
 
 
 
-
-
-const HomePageDiv = styled.div`
-margin-top: 10vmin;
+const CertificateSections = styled.section`
 width: 90vmax;
-background-color: #232732;
-box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
 display: flex;
-flex-direction: column;
+flex-direction: row;
 justify-content: center;
 align-items: center;
-border-radius: 24px;
+font-size: 90px;
+color: white;
 
 `;
-
-const Main = styled.div`
-width:80vmax;
+const ResumeSections = styled.section`
+width: 90vmax;
 display: flex;
-align-items: center;
+flex-direction: row;
 justify-content: center;
-flex-direction: column;
-gap: 40px;
-padding: 15vmin 40vmax ;
+align-items: center;
+font-size: 90px;
+color: white;
+
 `;
-const MainImg = styled.div`
-height: 30vmin;
-width: 30vmax;
+const AboutSections = styled.section`
+display: flex;
+flex-direction: row;
+align-items: center;
+gap:5vmax;
+justify-content: space-between;
+font-size: 90px;
+color: white;
 
 
 
-
-img{
-   width: 100%;
-   border-radius: 100%;
-   box-shadow:5px 5px 10px rgba(255, 255, 255, 0.25), -5px -5px 10px rgba(255, 255, 255, 0.25) ;
-    background-image: linear-gradient(rgba(255, 255, 255, 0.2),
-rgba(255, 255, 255, 0.2));
+@media screen and (max-width:700px){
+   flex-direction: column;
+   text-align: center;
+   
 }
 `;
 
+// const AboutImageDiv = styled.div`
+// height: 35vh;
+// width: 35vmax;
+
+// img{
+//    border-radius: 20px;
+//    width: 100%;
+//    box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
+   
+
+// }
+// `;
+
+const AboutImageDiv = styled.div`
+
+  height:40vmax;
+  width: 40vmax; 
+  border-radius: 20px; 
+  background-image: url('../Images/Boy.jpeg'); 
+  background-size: cover; 
+  background-position: center; 
+  box-shadow: 8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0, 0, 0, 0.25);
+
+`;
+
+const AboutMeIntroDuction = styled.div`
+width: 40vmax;
+display: flex;
+flex-direction: column;
+justify-content: center;
+gap: 30px;
+text-align: left;
+p{
+   margin: 0;
+   padding: 0;
+   font-size:21px;
+   font-weight: lighter;
+  
+  
+}
+h1{
+    margin: 0;
+   padding: 0;
+   font-size: 34px;
+   font-weight: 500;
+   white-space: nowrap;
+   
+}
+
+
+@media screen and (max-width:700px){
+   h1{
+      text-align: center;
+   }
+   p{
+       text-align: center;
+   }
+ 
+  
+   
+}
+
+
+`;
+
+const ProjectsSections = styled.section`
+width: 90vmax;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 90px;
+
+color: white;
+`;
+const ContactSections = styled.section`
+width: 90vmax;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 90px;
+color: white;
+
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const HomePageDiv = styled.div`
+// display: flex;
+// justify-content: center;
+// align-items: center;
+// margin: 2vmax;
+// background-color: #232732;
+// box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
+// border-radius: 24px;
+// border: none;
+// color: white;
+// `;
+
+// const Main = styled.div`
+// display: flex;
+// justify-content: center;
+// align-items: center;
+// flex-direction: column;
+// padding: 70px 100px;
+
+
+
+// `;
+
+// const MainImg = styled.div`
+// height: 474px;
+// width: 474px;
+// background-image: linear-gradient(rgba(255, 255, 255, 0.2),
+//  rgba(255, 255, 255, 0.2)),  url("../Images/MainLogo.jpg");
+// background-size: cover;
+// box-shadow:-5px -5px 10px rgba(255, 255, 255, 0.25), 5px 5px 10px rgba(255, 255, 255, 0.25) ;
+// border-radius: 100%;
+// background-position: center;
+// margin-top: 50px;
+// `;
