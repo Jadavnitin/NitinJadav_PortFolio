78573804dee4d1874d88b7dcbf0d544d085b7eef
Jadavnitin <nitinkumarjadav83@gmail.com>
2024-10-02 18:41:08 +0530

    Update

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import Header from './Header';
 import styled from 'styled-components';
-
+import githublinkimg from "/Images/Vector.png"
 
 
 const HomePage = () => {
@@ -18,9 +18,9 @@ const HomePage = () => {
             <Main>
                <Header refs={{ CertificateSection, ResumeSection, AboutSection, ProjectsSection, ContactSection }} />
                <MainImg>
-               
-                     {/* <img src={mainlogo} alt="mainlogo" /> */}
-            
+
+                  {/* <img src={mainlogo} alt="mainlogo" /> */}
+
                </MainImg>
                <Div>
                   <Hey>Hey</Hey>
@@ -55,9 +55,37 @@ const HomePage = () => {
                      </p>
                   </AboutMeIntroDuction>
                </AboutSections>
+
+
+               <ProjectsSections ref={ProjectsSection}>
+
+                  <ProjectTiltle>Projects</ProjectTiltle>
+                  <HoriZontalLine></HoriZontalLine>
+                  <ProjectButtonDiv>
+                     <button>HTML</button>
+                     <button>CSS</button>
+                     <button>JavaScript</button>
+                  </ProjectButtonDiv>
+                  <ProjectMainCard>
+                     <ProjectAllImage>
+                        <Projectimages1></Projectimages1>
+                        <Projectimages2></Projectimages2>
+                        <Projectimages3></Projectimages3>
+                     </ProjectAllImage>
+                     <Projectcard>
+                        <CardImg></CardImg>
+                        <CardTitle>Content Analyzer</CardTitle>
+                        <ProjectText> A robust tool designed for content creators, bloggers, and writers</ProjectText>
+                        <img src={githublinkimg} alt="githublinkimg" />
+                     </Projectcard>
+                  </ProjectMainCard>
+               </ProjectsSections>
+
+
+
+
                <CertificateSections ref={CertificateSection}>certificaate page</CertificateSections>
                <ResumeSections ref={ResumeSection}>Resume Page</ResumeSections>
-               <ProjectsSections ref={ProjectsSection}>Projects Page</ProjectsSections>
                <ContactSections ref={ContactSection}>Contact Page</ContactSections>
             </Main>
          </HomePageDiv>
@@ -66,8 +94,6 @@ const HomePage = () => {
 }
 
 export default HomePage
-
-
 
 const HomePageDiv = styled.section`
 margin-top: 10vmin;
@@ -92,37 +118,16 @@ gap: 40px;
 padding: 15vmin 40vmax ;
 
 `;
-// const MainImg = styled.div`
-
-// height: 35vh;
-// width: 35vmax;
-
-   
-//    img{
-//    width: 100%;
-//    border-radius: 100%;
-//   box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
-// }
-
-
-
-// `;
-
 
 const MainImg = styled.div`
   height: 40vmax;
   width: 40vmax; 
   border-radius: 100%; 
-  background-image: url('/Images/MainLogo.jpg');// Adjusted path
+  background-image: url('/Images/MainLogo.jpg');
   background-size: cover; 
   background-position: center; 
   box-shadow: 8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0, 0, 0, 0.25);
 `;
-
-
-
-
-
 
 const Hey = styled.p`
 margin: 0;
@@ -215,7 +220,6 @@ const ButtonContactMe = styled.button`
    font-size:22px;
    color: inherit;
 `;
-
 const Div = styled.div`
 display:flex;
 justify-content: center;
@@ -224,8 +228,6 @@ flex-direction: column;
 gap: 20px;
 
 `;
-
-
 
 const CertificateSections = styled.section`
 width: 90vmax;
@@ -264,19 +266,6 @@ color: white;
    
 }
 `;
-
-// const AboutImageDiv = styled.div`
-// height: 35vh;
-// width: 35vmax;
-
-// img{
-//    border-radius: 20px;
-//    width: 100%;
-//    box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
-   
-
-// }
-// `;
 
 const AboutImageDiv = styled.div`
   height: 40vmax;
@@ -328,15 +317,6 @@ h1{
 
 `;
 
-const ProjectsSections = styled.section`
-width: 90vmax;
-display: flex;
-justify-content: center;
-align-items: center;
-font-size: 90px;
-
-color: white;
-`;
 const ContactSections = styled.section`
 width: 90vmax;
 display: flex;
@@ -347,59 +327,159 @@ color: white;
 
 `;
 
+const ProjectsSections = styled.section`
+width: 80vmax;
+display: flex;
+flex-direction: column;
+justify-content: center;
+color: white;
+font-size: 34px;
+font-weight: 500;
+gap: 20px;
+
+`;
+
+const ProjectMainCard = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+margin-top: 80px;
+gap: 20px;
 
 
+@media screen and (max-width:760px){
+   flex-direction: column;
+   gap: 50px;
+}
 
+`;
+const Projectcard = styled.div`
+max-width: 465px;
+height:543px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+box-shadow: -8px -8px 15px rgba(255, 255, 255, 0.25), 8px 8px 10px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+gap: 20px;
 
+img{
+   margin-bottom: 1rem;
+}
 
+`;
+const CardImg = styled.div`
+width: 90%;
+height:60%;
+margin:20px 10px 0px 10px;
+background-image: url('/Images/Image_1.jpg');
+box-shadow: 4px 4px 7.5px rgba(255, 255, 255, 0.25), -4px -4px 7.5px rgba(0, 0, 0, 0.25);
+background-size: cover; 
+background-position: center; 
+border-radius: 16px;
 
+ 
+`;
 
+const CardTitle = styled.p`
+padding: 0;
+margin: 0;
+font-size: 26px;
+font-weight: 600;
 
+`;
+const ProjectText = styled.p`
+padding: 0;
+margin: 0;
+font-size: 17px;
+font-weight: lighter;
+text-align: center;
 
+`;
 
+const ProjectTiltle = styled.p`
+padding: 0;
+margin: 0;
+`;
+const HoriZontalLine = styled.div`
+width:100%;
+background: linear-gradient(to right, #7733FF 70%, #F7A21E 30%); /* Two colors split */
+padding: 5px;
+border-radius: 24px;
 
+`;
+const ProjectButtonDiv = styled.div`
+width: 100%;
+margin-top: 40px;
+display: flex;
+align-items: center;
+justify-content: space-between;
 
+button{
+   width: 15vmax;
+   height: 3vh;
+   box-sizing: content-box;
+   display: flex;   
+   padding: 4vmin 6vmin;
+   align-items: center;
+   justify-content: center;
+   border-radius: 24px;
+   border: none;
+   box-shadow:-5px -5px 10px rgba(255, 255, 255, 0.25), 5px 5px 10px rgba(0,0 ,0,25) ;
+   font-size:22px;
+   color: inherit;
+   background-color:#24272C;
+}
 
+`;
 
+const ProjectAllImage = styled.div`
+  width: 238px;
+  height: 688px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 
+  @media screen and (max-width: 760px) {
+    flex-direction: row;
+    gap: 50px;
+    width:100%;
+    height: auto;
+  }
+`;
 
+const Projectimages1 = styled.div`
+  width: 80%;
+  height: auto;
+  aspect-ratio: 1; 
+  background-image: url('/Images/Image_1.jpg');
+  background-size: cover; 
+  background-position: center; 
+  box-shadow: -8px -8px 15px rgba(255, 255, 255, 0.25), 8px 8px 15px rgba(0, 0, 0, 0.25);
+  border-radius: 100%; 
+`;
 
+const Projectimages2 = styled.div`
+  width: 80%;
+  height: auto;
+  aspect-ratio: 1; 
+  background-image: url('/Images/Image_2.jpg');
+  background-size: cover; 
+  background-position: center; 
+  box-shadow: -8px -8px 15px rgba(255, 255, 255, 0.25), 8px 8px 15px rgba(0, 0, 0, 0.25);
+  border-radius: 100%; 
+`;
 
-
-
-
-
-// const HomePageDiv = styled.div`
-// display: flex;
-// justify-content: center;
-// align-items: center;
-// margin: 2vmax;
-// background-color: #232732;
-// box-shadow:8px 8px 15px rgba(255, 255, 255, 0.25), -8px -8px 15px rgba(0,0 ,0,25);
-// border-radius: 24px;
-// border: none;
-// color: white;
-// `;
-
-// const Main = styled.div`
-// display: flex;
-// justify-content: center;
-// align-items: center;
-// flex-direction: column;
-// padding: 70px 100px;
-
-
-
-// `;
-
-// const MainImg = styled.div`
-// height: 474px;
-// width: 474px;
-// background-image: linear-gradient(rgba(255, 255, 255, 0.2),
-//  rgba(255, 255, 255, 0.2)),  url("../Images/MainLogo.jpg");
-// background-size: cover;
-// box-shadow:-5px -5px 10px rgba(255, 255, 255, 0.25), 5px 5px 10px rgba(255, 255, 255, 0.25) ;
-// border-radius: 100%;
-// background-position: center;
-// margin-top: 50px;
-// `;
+const Projectimages3 = styled.div`
+  width: 80%;
+  height: auto;
+  aspect-ratio: 1; 
+  background-image: url('/Images/Image_3.jpg');
+  background-size: cover; 
+  background-position: center; 
+  box-shadow: -8px -8px 15px rgba(255, 255, 255, 0.25), 8px 8px 15px rgba(0, 0, 0, 0.25);
+  border-radius: 100%; 
+`;
